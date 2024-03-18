b9bdf39811b2ead369de51d62363580642475e41
adrian-g-schmidt <adrian.g.schmidt@gmail.com>
2024-03-17 23:53:24 -0400

    add scroller button

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect, useRef} from 'react';
 import { Link, useLocation } from "react-router-dom";
+import Pointer from './assets/Pointer.js'; // Import the SVG file
 
 export default function Projects(){   
     const scrollRef = useRef(null);
@@ -8,13 +9,22 @@ export default function Projects(){
     const [displayLocation, setDisplayLocation] = useState(location);
     const [transitionStage, setTransistionStage] = useState("fadeIn");
     const [currentProject, setCurrentProject] = useState("");
+    const [scrollerShowing, setScrollerShowing] = useState('true')
+    const [scrollerColor, setScrollerColor] = useState('#FCFCFC')
+
+    const enterScroll = () => {
+        setScrollerColor('#D9D9D9')
+      };
+    
+    const leaveScroll = () => {
+    setScrollerColor('#FCFCFC')
+    };
 
     useEffect(() => {
         if (location.pathname !== displayLocation.pathname){
             setTransistionStage("fadeOut");
         } 
     }, [location, displayLocation]);
-
     
     useEffect(() => {
 
@@ -30,17 +40,19 @@ export default function Projects(){
             // Limit the new scrollTop value to the maximum scroll height
             const limitedScrollTop = Math.max(0, Math.min(maxScrollHeight, newScrollTop));
 
+            limitedScrollTop === maxScrollHeight ? setScrollerShowing(false) : setScrollerShowing(true);
+                
             container.scrollTop = limitedScrollTop;
-          };
+        };
       
-          // Add event listener to the container for the 'wheel' event
-          document.addEventListener('wheel', handleWheel, { passive: false });
-      
-          // Clean up the event listener when the component unmounts
-          return () => {
-            document.removeEventListener('wheel', handleWheel);
-          };
-        }, []); // Empty dependency array ensures this effect runs only once
+        // Add event listener to the container for the 'wheel' event
+        document.addEventListener('wheel', handleWheel, { passive: false });
+    
+        // Clean up the event listener when the component unmounts
+        return () => {
+        document.removeEventListener('wheel', handleWheel);
+        };
+    }, []); // Empty dependency array ensures this effect runs only once
       
 
     // Function to handle hover state change
@@ -52,6 +64,20 @@ export default function Projects(){
         setCurrentProject("");
     };
 
+
+    const scrollMore = () => {
+        const container = scrollRef.current;
+        const newScrollTop = scrollerShowing ? container.scrollTop + container.clientHeight : 0;
+        const maxScrollHeight = container.scrollHeight - container.clientHeight;
+
+        const limitedScrollTop = Math.max(0, Math.min(maxScrollHeight, newScrollTop));
+
+        limitedScrollTop === maxScrollHeight ? setScrollerShowing(false) : setScrollerShowing(true);
+        container.scrollTo({
+            top: limitedScrollTop,
+            behavior: 'smooth'
+        });
+    };
 
     return (
         <div className={`${transitionStage} body`}
@@ -71,7 +97,7 @@ export default function Projects(){
             </div>
 
             <div className='body-right projects firefox-scroll' ref={scrollRef}>
-                <h3 className='project-header'>Professional Work</h3>
+                <div className='project-header-parent'><h3 className='project-header'>Professional Work</h3><div className='project-count'>7</div></div>
                 <ul className='project-list' style={{marginBottom: "40px"}}>
                     <Link to="/projects/sonic" onMouseEnter={(e) => handleHover("SONIC MUTATIONS")} onMouseLeave={displayNone}><li>Sonic Mutations</li></Link>
                     <Link to="/projects/sonic" onMouseEnter={(e) => handleHover("SYSTEM OF A SOUND")} onMouseLeave={displayNone}><li>System of a Sound</li></Link>
@@ -81,7 +107,7 @@ export default function Projects(){
                     <Link to="/projects/sonic"><li>Australian Cybernetic Interactive Poster</li></Link>
                     <Link to="/projects/sonic"><li>Woroni Magazine Designs</li></Link>
                 </ul>
-                <h3 className='project-header'>Personal Work</h3>
+                <div className='project-header-parent'><h3 className='project-header'>Personal Work</h3><div className='project-count'>10</div></div>
                 <ul className='project-list'>
                     <Link to="/projects/sonic"><li>Machine Learning Bechdel Test</li></Link>
                     <Link to="/projects/sonic"><li>Laser Cut Iris</li></Link>
@@ -94,8 +120,14 @@ export default function Projects(){
                     <Link to="/projects/sonic"><li>Smart Loading Zones</li></Link>
                     <Link to="/projects/sonic"><li>Habit Helper</li></Link>
                 </ul>
+
+                <div onClick={scrollMore} onMouseEnter={enterScroll} onMouseLeave={leaveScroll} className='pointerParent'>
+                    <Pointer  color={scrollerColor} rotation = {scrollerShowing ? 'showPointer' : 'hidePointer'}></Pointer>
+                </div>
+                
             </div>
         </div>
+
     )
 }
 
